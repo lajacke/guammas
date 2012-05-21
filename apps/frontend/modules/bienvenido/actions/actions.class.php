ba20f2d57f77310b4e5648fda50b043b42df2a03
drake <drake@localhost>
2012-05-21 00:03:19 -0430

    car: creado el modulo bienvenido

---
+++ apps/frontend/modules/bienvenido/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * bienvenido actions.
+ *
+ * @package    guammas
+ * @subpackage bienvenido
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class bienvenidoActions extends sfActions {
+
+    /**
+     * Executes index action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeIndex(sfWebRequest $request) {
+        $this->setLayout("lay_bienvenido");
+    }
+
+}
