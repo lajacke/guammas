c25d8f41d525c9debf5524d246c42e03fed3f46d
drake <drake@localhost>
2012-05-21 00:02:21 -0430

    creado archivo de librerias js necesaria para la accion de login

---
+++ web/js/custom/login.script.js
@@ -0,0 +1,54 @@
+var loginFormHTML ='<form id="login_form" action="usuario/login" method="post"><div><h3>Inicia sesión</h3></div><div id="login_form_username">Usuario o Email:<br /><input type="text" name="login[username]" id="login_username" /></div><div id="login_form_password">Contraseña: <br /><input type="password" name="login[password]" id="login_password" /><br /></div></form>';
+var forgotFormHTML = '<form id="forgot_form" action="usuario/forgot" method="post"><div><h3>¿Olvidaste tu contraseña?</h3><br />Por favor, ingresa tu dirección de email. Reiniciaremos tu contraseña, te la enviaremos por correo y te daremos otras instrucciones. Luego podras cambiar la contraseña por la que desees!</div><hr /><div  id="login_form_username">Email:<br /><input type="text" name="forgot[email]" id="forgot_email" /></div><div></div>';
+var ajaxError = '<p><span class="ui-icon ui-icon-cancel" style="float:left; margin:0 7px 50px 0;"></span>Doh! ha ocurrido algo que no deberia!</p><p>Estamos trabajando por arreglarlo, danos un momemnto y algunas cervezas</b>.</p>';
+var btnArray={
+    "Iniciar Sesión": function() {
+        $("#login_form").submit();
+    },
+    "Crea una cuenta nueva!": function() {
+        $.ajax({
+            url: 'usuario/new',
+            statusCode: {
+                404: function() {
+                    $(".ui-dialog-buttonset").remove();
+                    $(this).html(ajaxError);
+                }
+            },
+            success: function(){
+                
+            },
+            error: function(){
+                $(".ui-dialog-buttonset").remove();
+                $(this).html(ajaxError);
+            }
+                
+        })
+    },
+    "Olvide mi contraseña": function() {
+        $(this).html(forgotFormHTML);
+        $(".ui-dialog-buttonset").remove();
+        $(this).dialog({
+            buttons: {
+                'Recuperar mi contraseña': function(){
+                    $("#forgot_form").submit();
+                }
+            }
+        })
+    }
+}
+
+$(document).ready(function(){
+    $("#main-login-form").dialog({
+        autoOpen: true,
+        modal: true,
+        width: 540,
+        open: function() {
+            $(this).html(loginFormHTML);
+            $(".ui-dialog-buttonset").remove();
+            $(this).dialog({
+                buttons: btnArray
+            })
+        },
+        buttons: btnArray
+    });
+})
