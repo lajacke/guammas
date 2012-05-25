778cb9541af2c1c28da267bb36e93c1490da8122
drake <drake@localhost>
2012-05-24 22:22:56 -0430

    quitados del formulario widgets innecesarios a la hora de crear cuentas nombre apellido actividad ultimo login activo y campos auditoria

--- lib/form/doctrine/UsuarioForm.class.php
+++ lib/form/doctrine/UsuarioForm.class.php
@@ -8,9 +8,18 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class UsuarioForm extends BaseUsuarioForm
-{
-  public function configure()
-  {
-  }
+class UsuarioForm extends BaseUsuarioForm {
+
+    public function configure() {
+        unset(
+            $this["nombre"], 
+            $this["apellido"], 
+            $this["actividad"], 
+            $this["ultimo_inicio_sesion"],
+            $this["activo"], 
+            $this["created_at"], 
+            $this["updated_at"]
+        );
+    }
+
 }
